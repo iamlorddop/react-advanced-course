e09f94a268ff9e9bb50cb1ea9eb539d46d6f2cc7
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-03-11 20:09:34 +0300

    end advanced react-stack course

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react"
-import {useSearchUsersQuery} from "../store/github/github.api";
+import {useLazyGetUserReposQuery, useSearchUsersQuery} from "../store/github/github.api";
 import {useDebounce} from "../hooks/debounce";
+import {RepoCard} from "../components/RepoCard";
 
 export function HomePage() {
     const [search, setSearch] = useState('')
@@ -10,13 +11,15 @@ export function HomePage() {
         skip: debounced.length < 3,
         refetchOnFocus: true
     })
+    const [fetchRepos, { isLoading: areReposLoading, data: repos }] = useLazyGetUserReposQuery()
 
     useEffect(() => {
         setDropdown(debounced.length > 3 && data?.length! > 0)
     }, [debounced, data])
 
     const clickHandler = (username: string) => {
-
+        fetchRepos(username)
+        setDropdown(false)
     }
 
     return (
@@ -32,6 +35,10 @@ export function HomePage() {
                         <li key={user.id} onClick={() => clickHandler(user.login)} className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer">{ user.login }</li>
                     ))}
                 </ul>}
+                <div className="container">
+                    { areReposLoading && <p className="text-center">Repos are loading...</p> }
+                    { repos?.map(repo => <RepoCard repo={repo} key={repo.id} />) }
+                </div>
             </div>
         </div>
     )
